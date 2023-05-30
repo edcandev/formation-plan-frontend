a157d28793cab53f4a64b2ed415e7a383faa6b20
edcan-dev <israel.da.queenie@gmail.com>
2023-05-30 14:19:55 -0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://edcan-dev.github.io/'
+  base: 'https://edcan-dev.github.io/formation-plan-frontend'
 })
