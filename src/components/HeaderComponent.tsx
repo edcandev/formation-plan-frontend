e5f3b9c558789897a97686a9a8736dd40fe48625
edcan-dev <israel.da.queenie@gmail.com>
2023-05-30 21:25:24 -0600

    https al backend

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -3,7 +3,7 @@ const HeaderComponent = () => {
   return (
     <>
     <div className="main-container-h1 py-4 text-center header-component-container">
-      <img src="/tese-logo.svg" className="header-component-container-img" />
+      <img src="tese-logo.svg" className="header-component-container-img" />
       <div className="header-component-container-div" >
       <h2 className="header-component-container-h2">Tecnológico de Estudios Superiores de Ecatepec</h2>
       <h1 className='main-container-h1 my-3 text-center header-component-container-h1'>  Generador de Planes de Formación</h1>
--- src/env/apiUrlHelper.ts
+++ src/env/apiUrlHelper.ts
@@ -1,6 +1,6 @@
 export function getAPI_URL(env: string) : string {
     //if(env == 'dev') return 'http://localhost:8080
-    if(env == 'dev') return 'https://formplanback.bmc4c7csbmh0e4ag.westus.azurecontainer.io'
+    if(env == 'dev') return 'https://tesegenerador.crdxbyasb5hbdhh5.eastus.azurecontainer.io'
     if(env == 'prod') return 'http://formationplanackend.cgb2gegzehhzg2ak.westus.azurecontainer.io';
     return '';
 }
