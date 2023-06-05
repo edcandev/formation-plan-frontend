5f408874c0c51cb47ed8d31bdeca1d868e06bd5f
Pedro Ivan <pedroivanlarios@gmail.com>
2023-06-05 11:28:13 -0600

    05/06/23

--- src/env/apiUrlHelper.ts
+++ src/env/apiUrlHelper.ts
@@ -1,6 +1,6 @@
 export function getAPI_URL(env: string) : string {
     //if(env == 'dev') return 'http://localhost:8080
-    if(env == 'dev') return 'https://generadordeplanes.centralus.azurecontainer.io'
+    if(env == 'dev') return 'https://generadordeplanes.centralus.azurecontainer.io';
     if(env == 'prod') return 'https://generadordeplanes.centralus.azurecontainer.io';
     return '';
 }
