44590c1e6ac6395f14b44d78b6d159269ffa5ef4
Pedro Ivan <pedroivanlarios@gmail.com>
2023-06-28 23:14:26 -0600

    cambios 28/06/22

--- src/env/apiUrlHelper.ts
+++ src/env/apiUrlHelper.ts
@@ -1,8 +1,6 @@
 export function getAPI_URL(env: string) : string {
-    if(env == 'dev') return 'https://generadordeplanes.centralus.azurecontainer.io'
-    if(env == 'dev') return 'http://localhost:8080'
-    if(env == 'dev') return 'https://tesegenerador.crdxbyasb5hbdhh5.eastus.azurecontainer.io'
-    if(env == 'prod') return 'http://formationplanackend.cgb2gegzehhzg2ak.westus.azurecontainer.io';
+    if(env == 'dev') return 'https://generadorteseinnova.centralus.azurecontainer.io'
+    if(env == 'prod') return 'http://generadorteseinnova.centralus.azurecontainer.io';
     return '';
 }
 // const ACI_URL = 'http://formationplanbackend.cgb2gegzehhzg2ak.westus.azurecontainer.io';
