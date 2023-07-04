ee0f8c476f77e803820da6d7f07d3de0228dd591
edcan-dev <israel.da.queenie@gmail.com>
2023-07-03 22:12:38 -0600

    fixed:mentor-select-list

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -3,25 +3,15 @@ const HeaderComponent = () => {
   return (
     <>
     <div className="main-container-h1 py-4 text-center header-component-container">
-<<<<<<< HEAD
-      <img src="tecnm-logo.png" className="header-component-container-img-tecnm-logo" />
-      <div className="header-component-container-div" >
-        <h2 className="header-component-container-h2">Tecnológico de Estudios Superiores de Ecatepec</h2>
-        <h1 className='main-container-h1 my-3 text-center header-component-container-h1'>  Generador de Planes de Formación<br/>Especialidad de Innovación Tecnológica</h1>
-      </div>
-      <img src="tese-logo.svg" className="header-component-container-img" />
-
-=======
     <img src="tecnm-logo.png" className="header-component-container-img-tecnm-logo" />
   <div className="header-component-container-div">
   <h2 className="header-component-container-h2">Tecnológico de Estudios Superiores de Ecatepec</h2>
-        <h1 className='main-container-h1 my-3 text-center header-component-container-h1'>  Generador de Planes de Formación<br/>Especialidad en Innovación Tecnológica</h1>
+        <h1 className='main-container-h1 my-3 text-center header-component-container-h1'>  Generador de Planes de Formación<br/>Especialidad Dual<br/>Innovación Tecnológica</h1>
   </div>
   <img src="logo-dual.png" className="header-component-container-img" />
 </div>
     <div className="main-container text-center banner-component-container">
       <img src="banner.png" className="banner-component-container-img" />
->>>>>>> b2ade02e7e15a3265289cfc7dc6e4582b48c3bda
     </div>
     </>
   )
